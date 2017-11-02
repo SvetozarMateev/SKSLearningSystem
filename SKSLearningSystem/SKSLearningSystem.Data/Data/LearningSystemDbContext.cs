33015b72eb926ee05c6c54a36a17bee25a156434
SvetozarMateev <vidic9898@gmail.com>
2017-11-02 18:17:52 +0200

    added data project

---
+++ SKSLearningSystem/SKSLearningSystem.Data/Data/LearningSystemDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using SKSLearningSystem.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SKSLearningSystem.Data
+{
+    public class LeatningSystemDbContext : IdentityDbContext<User>
+    {
+        public LeatningSystemDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static LeatningSystemDbContext Create()
+        {
+            return new LeatningSystemDbContext();
+        }
+    }
+}

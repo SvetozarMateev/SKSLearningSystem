3dda9d8ac3ad4af1cdc1ac00b26ea4ac19b00ad9
sdarakchiev <s.darakchiev@gmail.com>
2017-11-09 19:47:36 +0200

    Course slider

---
+++ SKSLearningSystem/SKSLearningSystem/Models/TakeCourseModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using SKSLearningSystem.Data.Models;
+
+namespace SKSLearningSystem.Models
+{
+    public class TakeCourseModel
+    {
+        public int Id { get; set; }
+
+        public string CourseName { get; set; }
+
+        public int CourseId { get; set; }
+
+        public string Description { get; set; }
+
+        public ICollection<Image> Images { get; set; }
+    }
+}

55f0da87b2df56b1a366364926de5f73151a8453
sdarakchiev <s.darakchiev@gmail.com>
2017-11-12 13:36:22 +0200

    Course Service unit test

---
+++ SKSLearningSystem/SKSLearningSystem.Tests/Controllers/CourseControllerTests/RenderImageShould.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SKSLearningSystem.Controllers;
+using SKSLearningSystem.Data;
+using SKSLearningSystem.Data.Models;
+using SKSLearningSystem.Services.CourseServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SKSLearningSystem.Tests.Controllers.CourseControllerTests
+{
+    [TestClass]
+    public class RenderImageShould
+    {
+        [TestMethod]
+        public async Task ReturnImageFile_WithCorrecParameters()
+        {
+            // Arrange
+            var contextMock = new Mock<LearningSystemDbContext>();
+            var courseServiceMock = new Mock<ICourseService>();
+
+            var image = new Image();
+            var currentImage = image.CurrentImage;
+            var imageId = image.Id;
+
+            var controller = new CourseController(courseServiceMock.Object, contextMock.Object);
+
+            // Act
+            await controller.RenderImage(imageId);
+
+            // Assert
+
+
+        }
+    }
+}

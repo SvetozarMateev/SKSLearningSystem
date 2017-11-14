d41fe7ed4bc472c72c603f10309301fc4bfabf98
sdarakchiev <s.darakchiev@gmail.com>
2017-11-14 20:41:23 +0200

    Home controller unit tests

---
+++ SKSLearningSystem/SKSLearningSystem.Tests/Controllers/HomeControllerTests/AllCoursesShould.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SKSLearningSystem.Controllers;
+using SKSLearningSystem.Data.Models;
+using SKSLearningSystem.Models.ViewModels;
+using SKSLearningSystem.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestStack.FluentMVCTesting;
+
+namespace SKSLearningSystem.Tests.Controllers.HomeControllerTests
+{
+    [TestClass]
+    public class AllCoursesShould
+    {
+        [TestMethod]
+        public void ReturnDefaultView_WithCorrectModel()
+        {
+            // Arrange
+            var homeServiceMock = new Mock<IHomeServices>();
+            var courses = new List<SingleCourseViewModel>();
+
+            homeServiceMock.Setup(x => x.GetCoursesFromDb()).Returns(courses);
+
+            var controller = new HomeController(homeServiceMock.Object);
+
+            
+            // Act & Assert
+            controller
+                .WithCallTo(c => c.AllCourses())
+                .ShouldRenderDefaultView();
+        }
+    }
+}
---
+++ SKSLearningSystem/SKSLearningSystem.Tests/Controllers/HomeControllerTests/MyProfilePost_Should.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SKSLearningSystem.Controllers;
+using SKSLearningSystem.Models.ViewModels;
+using SKSLearningSystem.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using TestStack.FluentMVCTesting;
+
+namespace SKSLearningSystem.Tests.Controllers.HomeControllerTests
+{
+    [TestClass]
+    public class MyProfilePost_Should
+    {
+        [TestMethod]
+        public void ReturnDefaultView_WithCorrectModel()
+        {
+            // Arrange
+            var homeServiceMock = new Mock<IHomeServices>();
+            var imageMock = new Mock<HttpPostedFileBase>();
+            var myProfileViewModel = new MyProfileViewModel();
+
+            homeServiceMock.Setup(h => h.ReadImagesFromFiles(imageMock.Object));
+
+            var controller = new HomeController(homeServiceMock.Object);
+
+            // Act & Assert
+            controller.WithCallTo(c => c.MyProfile(myProfileViewModel, imageMock.Object)).ShouldRenderDefaultView();
+        }
+    }
+}
